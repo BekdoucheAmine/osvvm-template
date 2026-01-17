756959e8713a5680e1e05b897c9d5817954b35ce
BekdoucheAmineAdel <amineadelbekdouche@gmail.com>
2026-01-17 18:24:30 +0100

    ci: moved from tcllib to tdom for handeling xml. tcllib doesn't support xml for now

--- .github/workflows/checkXML.tcl
+++ .github/workflows/checkXML.tcl
@@ -1,51 +1,38 @@
-package require xml
+package require tdom
 
+# Get arguments
 set xmlFile [lindex $argv 0]
 set DEBUG   [lindex $argv 1]
 
-# Read file into xmlData
+# Read XML file
 set fh [open $xmlFile r]
 set xmlData [read $fh]
 close $fh
 
-# assumes DEBUG is 0 or 1
-if {$DEBUG} {puts "\033[33mDEBUG\033[0m .xml file found : $xmlFile\n Parsing..."}
-
-set errors 1
-set failures 1
-set skipped 1
-
-proc startElement {name attList} {
-    global DEBUG
-
-    global errors
-    global failures
-    global skipped
-
-    array set attrs $attList
-
-    if {$DEBUG} {puts "\033[33mDEBUG\033[0m startElement \n name: $name \n attList: $attList)"}
-
-    if {$name eq "testsuites"} {
-        set errors $attrs(errors)
-        set failures $attrs(failures)
-        set skipped $attrs(skipped)
-
-        if {$DEBUG} {puts "\033[33mDEBUG\033[0m errors = $errors, failures = $failures, skipped = $skipped"}
-    }
+if {$DEBUG} {
+    puts "\033[33mDEBUG\033[0m .xml file found : $xmlFile\nParsing..."
 }
 
-xml::parser p -elementstartcommand startElement
-p parse $xmlData
+# Parse XML using tdom
+set doc  [dom parse $xmlData]
+set root [$doc documentElement]  ;# <testsuites>
+
+# Extract attributes
+set errors   [$root getAttribute errors 0]
+set failures [$root getAttribute failures 0]
+set skipped  [$root getAttribute skipped 0]
+
+if {$DEBUG} {
+    puts "\033[33mDEBUG\033[0m errors=$errors, failures=$failures, skipped=$skipped"
+}
+
+# Print results
+puts ".xml File: $xmlFile"
 
 if {$errors != 0 || $failures != 0 || $skipped != 0} {
-    puts ".xml File: $xmlFile"
-    puts " \033\[31mBuild: FAILLED\033[0m, Errors: $errors, Failures: $failures, Skipped: $skipped";
-    set exit_code 1
+    puts "\033[31mBuild: FAILED\033[0m, Errors: $errors, Failures: $failures, Skipped: $skipped"
+    exit 1
 } else {
-    puts ".xml File: $xmlFile"
-    puts " \033[32mBuild: PASSED\033[0m, Errors: $errors, Failures: $failures, Skipped: $skipped";
-    set exit_code 0
+    puts "\033[32mBuild: PASSED\033[0m, Errors: $errors, Failures: $failures, Skipped: $skipped"
+    exit 0
 }
-
-exit $exit_code
