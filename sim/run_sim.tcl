537947f603bcb586642ed7f57357181b49468630
BekdoucheAmineAdel <amineadelbekdouche@gmail.com>
2026-01-17 17:42:22 +0100

    fix: removed checkXML usage (moved to ci workflows)

--- sim/run_sim.tcl
+++ sim/run_sim.tcl
@@ -1,23 +1,8 @@
-# Set DEBUG to 0
-set DEBUG 0
-
 # Build OSVVM 
 build build.pro
 
-# Specify xml file location
-set xmlFile sim_build/sim_build.xml
-
-# Check Build Status
-source checkXML.tcl
-
 # Run All Tests
 build RunAllTests.pro
-
-# Specify xml file location
-set xmlFile sim_RunAllTests/sim_RunAllTests.xml
-
-# Check Simulation Status
-source checkXML.tcl
 
 # Exit ModelSim
 quit -code 0 -f
